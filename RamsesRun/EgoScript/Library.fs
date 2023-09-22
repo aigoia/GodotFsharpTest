550dd16b8e514f83cb8ba7f915550f07e3ca8c57
aigoia <oidoicompany@gmail.com>
2023-09-22 15:41:12 +0900

    cleaning

--- RamsesRun/EgoScript/Library.fs
+++ RamsesRun/EgoScript/Library.fs
@@ -4,3 +4,11 @@ open System
 
 module Init =
     let say = fun() -> GD.Print("Hello Godot!")
+
+    let saySwitch (say : string) =
+        match say.ToLower() with
+        | "hello world!" -> "Nice to meet you!"
+        | "hello world" -> "Nice to meet you"
+        | "hello godot!" -> "Good to see you again!"
+        | "hello godot" -> "Good to see you again"
+        | _ -> say
--- RamsesRun/Main.cs
+++ RamsesRun/Main.cs
@@ -1,9 +1,15 @@
+using System.Linq;
 using Godot;
+using EgoScript;
 
 namespace RamsesRun;
 
 public partial class Main : Node2D
 {
+	public override void _Ready()
+	{
+	}
+	
 	public override void _Process(double delta)
 	{
 		if (Input.IsKeyPressed(Key.Escape)) GetTree().Quit();
